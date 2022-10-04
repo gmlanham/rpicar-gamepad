d23a9297ffbd559f01c9fe89ed719e04b3373e69
Mike Lanham <m_lanham@hotmail.com>
2022-10-04 09:52:57 -0700

    Add files via upload

--- motor_module.py
+++ motor_module.py
@@ -19,16 +19,13 @@ Motor_B_Pin2  = 18
 
 Dir_forward   = 0
 Dir_backward  = 1
-# 
-# left_forward  = 1
-# left_backward = 0
-# 
-# right_forward = 0
-# right_backward= 1
 
-speed_set = 0
+motor_freq = 100
+speed_lower_limit = 1.0
+#speed_set = 0
 left_speed_set = 0
 right_speed_set = 0
+
 def motorStop():#Motor stops
     GPIO.output(Motor_A_Pin1, GPIO.LOW)
     GPIO.output(Motor_A_Pin2, GPIO.LOW)
@@ -47,43 +44,44 @@ def init():
     GPIO.setup(Motor_B_Pin2, GPIO.OUT)    
     motorStop()
     try:
-        pwm_A = GPIO.PWM(Motor_A_EN, 1000)
-        pwm_B = GPIO.PWM(Motor_B_EN, 1000)
+        pwm_A = GPIO.PWM(Motor_A_EN, motor_freq)
+        pwm_B = GPIO.PWM(Motor_B_EN, motor_freq)
     except:
         pass
 
 def motor_left(status, direction, left_speed_set):
-    if status == 0: # stop
-        GPIO.output(Motor_B_Pin1, GPIO.LOW)
-        GPIO.output(Motor_B_Pin2, GPIO.LOW)
-        GPIO.output(Motor_B_EN, GPIO.LOW)
-    else:
-        if direction == Dir_backward:
-            GPIO.output(Motor_B_Pin1, GPIO.HIGH)
-            GPIO.output(Motor_B_Pin2, GPIO.LOW)
-            pwm_B.start(1)
-            pwm_B.ChangeDutyCycle(left_speed_set)
-        elif direction == Dir_forward:
+    if (left_speed_set < 100 and left_speed_set > speed_lower_limit):
+        if status == 0: # stop
             GPIO.output(Motor_B_Pin1, GPIO.LOW)
-            GPIO.output(Motor_B_Pin2, GPIO.HIGH)
-            pwm_B.start(0)
-            pwm_B.ChangeDutyCycle(left_speed_set)
+            GPIO.output(Motor_B_Pin2, GPIO.LOW)
+            GPIO.output(Motor_B_EN, GPIO.LOW)
+        else:
+            if direction == Dir_backward:
+                GPIO.output(Motor_B_Pin1, GPIO.HIGH)
+                GPIO.output(Motor_B_Pin2, GPIO.LOW)
+                pwm_B.start(1)
+                pwm_B.ChangeDutyCycle(left_speed_set)
+            elif direction == Dir_forward:
+                GPIO.output(Motor_B_Pin1, GPIO.LOW)
+                GPIO.output(Motor_B_Pin2, GPIO.HIGH)
+                pwm_B.start(0)
+                pwm_B.ChangeDutyCycle(left_speed_set)
+            
 
 def motor_right(status, direction, right_speed_set):
-    if status == 0: # stop
-        GPIO.output(Motor_A_Pin1, GPIO.LOW)
-        GPIO.output(Motor_A_Pin2, GPIO.LOW)
-        GPIO.output(Motor_A_EN, GPIO.LOW)
-    else:
-        if direction == Dir_backward:
-            #print(direction)            
+    if (right_speed_set < 100 and right_speed_set > speed_lower_limit):
+        if status == 0: # stop
             GPIO.output(Motor_A_Pin1, GPIO.LOW)
-            GPIO.output(Motor_A_Pin2, GPIO.HIGH)
-            pwm_A.start(1)
-            pwm_A.ChangeDutyCycle(right_speed_set)
-        elif direction == Dir_forward:
-            #print(direction)            
-            GPIO.output(Motor_A_Pin1, GPIO.HIGH)
             GPIO.output(Motor_A_Pin2, GPIO.LOW)
-            pwm_A.start(0)
-            pwm_A.ChangeDutyCycle(right_speed_set)
+            GPIO.output(Motor_A_EN, GPIO.LOW)
+        else:
+            if direction == Dir_backward:           
+                GPIO.output(Motor_A_Pin1, GPIO.LOW)
+                GPIO.output(Motor_A_Pin2, GPIO.HIGH)
+                pwm_A.start(1)
+                pwm_A.ChangeDutyCycle(right_speed_set)
+            elif direction == Dir_forward:          
+                GPIO.output(Motor_A_Pin1, GPIO.HIGH)
+                GPIO.output(Motor_A_Pin2, GPIO.LOW)
+                pwm_A.start(0)
+                pwm_A.ChangeDutyCycle(right_speed_set)
--- rpicar_F710.py
+++ rpicar_F710.py
@@ -12,13 +12,10 @@ import inputs #gamepad
 import RPi.GPIO as GPIO
 from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
-#from webstreaming_module import *
+
 import usonic_module
 import motor_module
 
-#import webstreaming_module
-#import camera_module
-#camera_module.rpicamera()
 usonic_module.checkdist()
 
 print(__file__)
@@ -29,7 +26,7 @@ print("Blocking mode changed")
 print("Blocking Mode:", os.get_blocking(fd))
 
 factory = PiGPIOFactory()
-#the servo gpio is 3. BOARD pin number is 5,
+#the servo GPIO is 3. BOARD pin number is 5,
 servo = Servo(3, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 
 servoPIN = 3
@@ -38,9 +35,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-print("Start servo mid range.")
 servo.value=0
 sleep(1)
+
+speed_lower_limit = 1.0
 
 pads = inputs.devices.gamepads
 
@@ -50,71 +48,64 @@ left_backward = 0
 right_forward = 0
 right_backward= 1
 
+right_speed_set = 0.0
+left_speed_set = 0.0
+
 if len(pads) == 0:
     raise Exception("Couldn't find any Gamepads!")
 
 motor_module.init()
 
-while True:
-    
-    try:
-        
+while True:   
+    try:        
         events = inputs.get_gamepad()
         for event in events:
             #print(event.code, event.state)
-            if event.code == 'BTN_SELECT' and event.state == 0:             #9
+            if event.code == 'BTN_SELECT' and event.state == 0:             #9, stop motors
                 print("exit, break")
                 sys.exit()
                 break # if CTRL+C is pressed         
-#                 camera.stop_preview()
-#                 camera.stop_recording()
-#                 camera.close() # stop camera
-#                 time.sleep(0.01)
-            if event.code == 'BTN_START' and event.state == 0:             #10
+
+            if event.code == 'BTN_START' and event.state == 0:             #10, start motors
                 print('streaming browser 192.168.0.11')
                 usonic_module.checkdist()
                 time.sleep(0.01)
-            if event.code == 'ABS_RY':                                     #axis3 Rstick Vert                
-                #print(('RY',(event.state-327.68)/327.68))
-                right_speed_set = (abs(event.state)/327.68)          #action: set right motor speed
-                if ((event.state-327.68)/327.68 < -50):
-                    motor_module.motor_right(1, right_forward, right_speed_set)
-                elif ((event.state-327.68)/327.68 > 50):
-                    motor_module.motor_right(1, right_backward, right_speed_set)
+
+            if event.code == 'ABS_RX':                                     #axis3 Rstick, steer                
+                right_speed_set = (abs(event.state)/350)/2                 #action: set steering speed, slower
+                if ((event.state-327.68)/327.68 < -speed_lower_limit):     
+                    motor_module.motor_right(1, right_forward, right_speed_set+left_speed_set)
+                    motor_module.motor_left(1, left_backward, left_speed_set+right_speed_set)
+                elif ((event.state-327.68)/327.68 > speed_lower_limit):
+                    motor_module.motor_right(1, right_backward, right_speed_set+left_speed_set)
+                    motor_module.motor_left(1, left_forward, left_speed_set+right_speed_set)
                 else:
                     motor_module.motorStop()
-            if event.code == 'ABS_X': #camera servo, axis2 Lstick Horiz
+                    
+            if event.code == 'ABS_RY':                                    #axis1 Rstick Vert, move straight
+                left_speed_set = (abs(event.state)/350)                   #action: set motor speed
+                if ((event.state-327.68)/327.68 < -speed_lower_limit):
+                    motor_module.motor_left(1, left_forward, left_speed_set-right_speed_set) # Y rotational transform to make forward straight up
+                    motor_module.motor_right(1, right_forward, right_speed_set+left_speed_set) # X rotational transform to make forward straight up
+                    motor_module.motor_left(1, left_backward, left_speed_set-right_speed_set)
+                    motor_module.motor_right(1, right_backward, right_speed_set+left_speed_set)
+                else:
+                    motor_module.motorStop()                            #this stops motors after each time cycle, when commented-out continous motor action allowed.
+            
+            if event.code == 'ABS_X':                                   #axis2 Lstick Horiz, camera servo
                 deltaX=(event.state)/32768
-                #print('event.state=',event.state,'deltaX=',deltaX)
-                #time.sleep( 0.1 )
-                #print(event.state)
-                #time.sleep( 0.1 )
-                #print('ABS_X',event.state)
-                print('-deltaX',-deltaX)
                 if abs(deltaX)> 0.1:                  
                     servo.value= -deltaX
                     time.sleep( 0.01 )
-            if event.code == 'ABS_Y':                                      #axis1 Lstick Vert
-                #print('ABS_Y',event.state)
-                #print(('Y',(event.state-327.68)/327.68))
-                left_speed_set = (abs(event.state)/327.68)            #action: set left motor speed
-                #print(left_speed_set)
-                if ((event.state-327.68)/327.68 < -50):
-                    motor_module.motor_left(1, left_forward, left_speed_set)
-                elif ((event.state-327.68)/327.68 > 50):
-                    motor_module.motor_left(1, left_backward, left_speed_set)
-                else:
-                    motor_module.motorStop()                                            #this stops motors after each time cycle, when commented-out continous motor action allowed.
-            
+
     except KeyboardInterrupt:
-        #camera_module.camera_close()
         motor_module.motorStop() 
         print("break on CTRL+c")
-        break # if CTRL+C is pressed
+        break # when CTRL+C is pressed
 
 def destroy():
     motor_module.motorStop()
-    GPIO.cleanup()             # Release resource
+    GPIO.cleanup() # Release resources
 
 
 
